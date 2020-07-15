0dc66f77caf36ea86bc4c716e656146f9acb46ad
Luis Marquezzini <marquezzini@dcc.ufrj.br>
2020-07-15 16:36:49 -0300

    Starting upload and env

--- index.js
+++ index.js
@@ -10,6 +10,28 @@ app.use(express.json());
 
 const productRoutes = require('./src/routes/productRoute');
 
+// MULTER
+const multer = require('multer')
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function(req, file, cb) {
+    console.log(file)
+    cb(null, file.originalname)
+  }
+})
+
+app.post('/api/v1/upload', (req, res, next) => {
+    const upload = multer({ storage }).single('name-of-input-key')
+    upload(req, res, function(err) {
+      if (err) {
+        return res.send(err)
+      }
+      res.json(req.file)
+    })
+})
+
 app.use('/api/v1', productRoutes);
 
 
